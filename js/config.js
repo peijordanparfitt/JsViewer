60435b1c63f9874d0fb32aaab9f0fbfdf400c756
peijordanparfitt <jparfitt@patrickco.com>
2013-05-07 15:35:04 -0600

    First Try

---
+++ js/config.js
@@ -0,0 +1,44 @@
+﻿var initialCenter = [-81.3794, 28.5381]
+var intialZoom = 10
+
+
+
+var idents = [
+        {   
+            layerName: "OrlandoSplicesPt",
+            layerContent:"NAME: ${ORL-EN-00096} <br>" + 
+            "EA2: ${FLORIDA} <br>" + 
+            "NAME_1: ${Seminole} <br>" + 
+            "COUNTY_KEY: ${COUNTY_KEY} <br>"
+        },
+        {
+            layerName: "OrlandoSlacksPt",
+            layerContent: "NAME: ${NAME} <br>" + 
+            "EA2: ${EA2} <br>" + 
+            "LOCATEID: ${LOCATEID} <br>" + 
+            "OWNER: ${OWNER} <br>" + 
+            "MEDIUM: ${MEDIUM} <br>" + 
+            "MANUFACTUR: ${MANUFACTUR} <br>"
+        },
+        {
+            layerName: "OrlandoAPPt",
+            layerContent: "NAME: ${NAME} <br>" + 
+            "ADDRESSNOT: ${ADDRESSNOT} <br>" +
+            "PLACEMENT: ${PLACEMENT} <br>" + 
+            "OWNER: ${OWNER} <br>" + 
+            "EA2: ${EA2} <br>" + 
+            "NAME_1: ${NAME_1} <br>"
+        },
+        {
+            layerName: "OrlandoMetro",
+            layerContent: "NAME: ${NAME} <br>" +
+            "PLACEMENT: ${PLACEMENT} <br>" +
+            "EA2: ${EA2} <br>" +
+            "OWNER: ${OWNER} <br>" +
+            "MEDIUM: ${MEDIUM} <br>" +
+            "MANUFACTUR: ${MANUFACTUR} <br>"
+        }
+    ];
+
+
+
